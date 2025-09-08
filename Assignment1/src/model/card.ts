adfbd2ff64c7a3258bc7c781959ead08db64fb91
EmanuelDuca <edu@usekeyhole.com>
2025-09-08 18:04:01 +0200

    add player.ts file

--- Assignment1/src/model/card.ts
+++ Assignment1/src/model/card.ts
@@ -1,33 +1,24 @@
-export type Color =
-    | 'RED'
-    | 'YELLOW'
-    | 'GREEN'
-    | 'BLUE'
+export type Color = "RED" | "YELLOW" | "GREEN" | "BLUE";
 
 export type Type =
-    | 'NUMBERED'
-    | 'SKIP'
-    | 'REVERSE'
-    | 'DRAW'
-    | 'WILD'
-    | 'WILD DRAW'
+  | "NUMBERED"
+  | "SKIP"
+  | "REVERSE"
+  | "DRAW"
+  | "WILD"
+  | "WILD DRAW";
 
 export type Numbered = {
-    readonly type: 'NUMBERED',
-    readonly color: Color,
-    readonly number: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
-}
+  readonly type: "NUMBERED";
+  readonly color: Color;
+  readonly number: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+};
 
 export type ColoredAction =
-    | { readonly type: 'SKIP', readonly color: Color }
-    | { readonly type: 'REVERSE', readonly color: Color }
-    | { readonly type: 'DRAW', readonly color: Color }
+  | { readonly type: "SKIP"; readonly color: Color }
+  | { readonly type: "REVERSE"; readonly color: Color }
+  | { readonly type: "DRAW"; readonly color: Color };
 
-export type Wild =
-    | { readonly type: 'WILD' }
-    | { readonly type: 'WILD DRAW' }
+export type Wild = { readonly type: "WILD" } | { readonly type: "WILD DRAW" };
 
-export type Card =
-    | Numbered
-    | ColoredAction
-    | Wild
+export type Card = Numbered | ColoredAction | Wild;
